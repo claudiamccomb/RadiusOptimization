86400a9cf304da30930e9179a25c2e0f07c56f4e
claudiamccomb <cmccomb1203@gmail.com>
2024-06-03 15:48:00 -0400

    add functionality for cartesian coordinates as well as polar

--- radiusOptimization.cpp
+++ radiusOptimization.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 using namespace Eigen;
 
-// Fit a circle to an array of radii using least squares method
-tuple<double, double, double> fitCircle(const vector<double>& radii, const vector<double>& angles) {
+// Fit polar coordinates to a circle of best fit
+tuple<double, double, double> fitCircleFlat(const vector<double>& radii, const vector<double>& angles) {
     int n = radii.size();
     MatrixXd A(n, 3);
     VectorXd b(n);
@@ -32,9 +32,32 @@ tuple<double, double, double> fitCircle(const vector<double>& radii, const vecto
     return make_tuple(x0, y0, radius);
 }
 
+// Fit round, cartesian points to a circle of best fit
+// Change original x and z axes to x and y within this function for simplicity
+tuple<double, double, double> fitCircleRound(const vector<double>& x_values, const vector<double>& y_values) {
+    int n = x_values.size();
+    MatrixXd A(n, 3);
+    VectorXd b(n);
+
+    for (int i = 0; i < n; ++i) {
+        A(i, 0) = -2 * x_values[i];
+        A(i, 1) = -2 * y_values[i];
+        A(i, 2) = 1;
+        b(i) = x_values[i] * x_values[i] + y_values[i] * y_values[i];
+    }
+
+    MatrixXd aT = A.transpose();
+    VectorXd solution = (aT * A).colPivHouseholderQr().solve(aT * b);
+
+    double x0 = solution(0);
+    double y0 = solution(1);
+    double radius = sqrt(abs(x0 * x0 + y0 * y0 - solution(2)));
+
+    return make_tuple(x0, y0, radius);
+}
 
 int main() {
-    ifstream file("C:/Users/claud/Downloads/outputLog.txt");
+	ifstream file("outputCenteredLogRound.txt");
     map<double, vector<double>> column_groups_first;
     map<double, vector<double>> column_groups_third;
     vector<double> radius;
@@ -52,10 +75,10 @@ int main() {
 
     for (const auto& pair : column_groups_first) {
         double key = pair.first;
-        vector<double> radii = pair.second;
-        vector<double> angles = column_groups_third[key];
+        vector<double> x_values = pair.second;
+        vector<double> z_values = column_groups_third[key];
 
-        auto circleParams = fitCircle(radii, angles);
+		auto circleParams = fitCircleRound(x_values, z_values); // choose either fitCircleFlat or fitCircleRound depending on input data
         cout << "Circle Center (x0, y0): " << get<0>(circleParams) << ", " << get<1>(circleParams) << endl;
         cout << "Circle Radius (r): " << get<2>(circleParams) << endl;
         radius.push_back(get<2>(circleParams));
