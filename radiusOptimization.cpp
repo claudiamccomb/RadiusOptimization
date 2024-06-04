ac25d38f3dcf9dde80e542681a6e45f579e8eea6
claudiamccomb <cmccomb1203@gmail.com>
2024-06-04 13:56:17 -0400

    integrated line of best fit from the centroid

--- radiusOptimization.cpp
+++ radiusOptimization.cpp
@@ -4,6 +4,7 @@
 #include <Eigen/Dense>
 #include <fstream>
 #include <map>
+#include <iomanip>
 
 using namespace std;
 using namespace Eigen;
@@ -56,22 +57,69 @@ tuple<double, double, double> fitCircleRound(const vector<double>& x_values, con
     return make_tuple(x0, y0, radius);
 }
 
+// Calculate the line of best fit within the object
+vector<Vector3d> lineOfBestFit(const vector<Vector3d>& points, const string& filename) {
+    Vector3d centroid = Vector3d::Zero();
+    Matrix3d covariance = Matrix3d::Zero();
+    ofstream outputFile(filename);
+
+    if (!outputFile.is_open()) {
+        cerr << "Error: Unable to open file for writing." << endl;
+    }
+
+    for (const auto& point : points) {
+        centroid += point;
+    }
+    centroid /= points.size();
+
+    for (const auto& point : points) {
+        Vector3d deviation = point - centroid;
+        covariance += deviation * deviation.transpose();
+    }
+    covariance /= points.size();
+
+    // Perform SVD and extract the direction vector of the line from the first column of the U matrix
+    JacobiSVD<MatrixXd> svd(covariance, ComputeThinU | ComputeThinV);
+    Vector3d direction_vector = svd.matrixU().col(0);
+
+    cout << "Direction vector of the line:\n" << direction_vector << endl;
+    cout << "Centroid of the point cloud:\n" << centroid << endl;
+
+    for (double y = -360.0; y <= 360.0; y += 1.2) { // Adjust spacing as needed
+        double x = centroid.x() + direction_vector.x() * y;
+        double z = centroid.z() + direction_vector.z() * y;
+        outputFile << std::setprecision(10) << x << " " << y << " " << z << endl;
+    }
+
+    outputFile.close();
+    cout << "Points written to file: " << filename << endl;
+    vector<Vector3d> result = { direction_vector , centroid };
+
+    return result;
+}
+
 int main() {
 	ifstream file("outputCenteredLogRound.txt");
     map<double, vector<double>> column_groups_first;
     map<double, vector<double>> column_groups_third;
+    vector<Vector3d> data_points;
     vector<double> radius;
     double smallest_radius = 0;
 
     for (string line; getline(file, line);) {
         istringstream iss(line);
         double first_value, second_value, third_value;
+        Vector3d temp_vector;
         // group data by rings on the y-axis
         if (iss >> first_value >> second_value >> third_value) {
             column_groups_first[second_value].push_back(first_value);
             column_groups_third[second_value].push_back(third_value);
+			temp_vector << first_value, second_value, third_value;
         }
+		data_points.push_back(temp_vector);
     }
+
+	vector<Vector3d> LBF = lineOfBestFit(data_points, "lineOfBestFitRound.txt");
 
     for (const auto& pair : column_groups_first) {
         double key = pair.first;
